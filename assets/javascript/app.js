5a022f6335435a3d7ee5d585c1fa1219a0582bf2
Hamed Rohani <hamedrohani@Hameds-MacBook-Pro.local>
2018-03-29 11:11:06 -0700

    1

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyANNhzqVYqoSeLoMatGOb5YbIANBOR8ERo",
+    authDomain: "traveljournal-14d81.firebaseapp.com",
+    databaseURL: "https://traveljournal-14d81.firebaseio.com",
+    projectId: "traveljournal-14d81",
+    storageBucket: "traveljournal-14d81.appspot.com",
+    messagingSenderId: "474695408824"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//cancel button
+$("#cancel-button").click(function(){
+    alert("you clicked cancel.");
+});
+
+//id='submit-button' on click
+$("#submit-button").click(function(){
+    // alert("you clicked submit.");
+    
+    var title = $("#input-title").val().trim();
+    var content = $("#input-content").val().trim();
+    var lon = $("#input-lon").val().trim();
+    var lat = $("#input-lat").val().trim();
+    var temp = $("#input-temp").val().trim();
+    var w_condition = $("#input-w-condition").val().trim();
+
+//data validation:
+if(title !=="" && content !==""){
+
+    //make an object
+    var dataObject = {
+        title: title,
+        content: content,
+        lon: lon,
+        lat: lat,
+        temp: temp,
+        w_coidtion: w_condition
+    }
+
+    //save it to firebase
+    database.ref().push(dataObject);
+
+    //clean the form
+    $("#input-title").val("");
+    $("#input-content").val("");
+    $("#input-lon").val("");
+    $("#input-lat").val("");
+    $("#input-temp").val("");
+    $("#input-w-condition").val("");
+    //focus on the first line
+    $("#input-title").focus();
+    alert("saved")
+
+
+
+}
+});
