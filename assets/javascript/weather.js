d2b15208ccb3e60a9fccf3077f3fcbca28ad8ac8
Hamed Rohani <hamedrohani@Hameds-MacBook-Pro.local>
2018-04-02 11:08:03 -0700

    new changes for weather

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,17 +1,29 @@
 
-var lon = 37.8267;
-var lat = -122.4233;
-var data = lon +","+ lat;
-var fullURL = "https://api.darksky.net/forecast/9d227d38df82a82af5c132e981501faf/" + data + "?extend=hourly&callback=?";
-// var proxyurl = "https://proxy-cbc.herokuapp.com/proxy";
-// $.ajax({
-//     url: fullURL,
-//     method: "GET"
-// }).then(function(response){
-//     console.log(response);
-// })
+
+/// just for demonstration purpose:
+var lon = 39.721846;
+var lat = -91.5154482;
+    
+getWeather(lon, lat);
+
+function getWeather(lon, lat){
+    var object = {
+        "F": null,
+        "Status": null
+        };
+var f = lon +","+ lat;
+var fullURL = "https://api.darksky.net/forecast/9d227d38df82a82af5c132e981501faf/" + f + "?extend=hourly&callback=?";
 
 
 $.getJSON(fullURL, function(data){
-    console.log(data);
+    // console.log(data);
+    object.F = data.currently.temperature;
+    object.Status = data.currently.summary;
+    $("#input-temp").val(object.F);
+    $("#input-w-condition").val(object.Status);
+    
 });
+
+}
+
+
