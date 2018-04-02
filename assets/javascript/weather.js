6fe765d58e396617d83e63eb6a01b9ea5fc57eb3
Yash Arya <yasharya11@gmail.com>
2018-04-02 14:51:02 -0700

    update

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,29 +1,25 @@
-
-
 /// just for demonstration purpose:
 var lon = 39.721846;
 var lat = -91.5154482;
-    
+
 getWeather(lon, lat);
 
-function getWeather(lon, lat){
+function getWeather(lon, lat) {
     var object = {
         "F": null,
         "Status": null
-        };
-var f = lon +","+ lat;
-var fullURL = "https://api.darksky.net/forecast/9d227d38df82a82af5c132e981501faf/" + f + "?extend=hourly&callback=?";
+    };
+    var f = lon + "," + lat;
+    var fullURL = "https://api.darksky.net/forecast/9d227d38df82a82af5c132e981501faf/" + f + "?extend=hourly&callback=?";
 
 
-$.getJSON(fullURL, function(data){
-    // console.log(data);
-    object.F = data.currently.temperature;
-    object.Status = data.currently.summary;
-    $("#input-temp").val(object.F);
-    $("#input-w-condition").val(object.Status);
-    
-});
+    $.getJSON(fullURL, function (data) {
+        // console.log(data);
+        object.F = data.currently.temperature;
+        object.Status = data.currently.summary;
+        $("#input-temp").val(object.F);
+        $("#input-w-condition").val(object.Status);
+
+    });
 
 }
-
-
