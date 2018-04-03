25ec31d4ba77afb6fe85b1df988433a64e1ed173
Hamed Rohani <hamedrohani@Hameds-MacBook-Pro.local>
2018-04-03 10:44:06 -0700

    new changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -133,3 +133,28 @@ $(document).on("click", ".articleBTN", function(event){
 });
     $("#listOfJournals").prepend(containerDiv);
 })
+
+
+// weather and geolocation code
+var url = "http://ip-api.com/json"
+$.ajax({
+    url: url,
+    method: "GET"
+}).then(function (data) {
+console.log(data);
+    //Lat
+    var lat = data.lat
+    //Lon
+    var lon = data.lon
+    $("#input-lat").val(lat);
+    $("#input-lon").val(lon);
+
+    var fullURL = "https://api.darksky.net/forecast/9d227d38df82a82af5c132e981501faf/" + lat + "," + lon + "?extend=hourly&callback=?";
+    $.getJSON(fullURL, function (data2) {
+        $("#input-temp").val(data2.currently.temperature);
+        $("#input-w-condition").val(data2.currently.summary);
+
+    });
+
+
+});
